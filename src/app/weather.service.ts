2c8ecf8658a80d566ebf010bfacba6afc38d18f0
asankaruwanh <asankaruwanh@gmail.com>
2024-04-28 16:28:33 -0500

    comitting changes

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,16 +1,60 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { Observable } from 'rxjs/internal/Observable';
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
   private apiKey: string = '482944e26d320a80bd5e4f23b3de7d1f';
-  private apiUrl: string = 'https://api.openweathermap.org/data/2.5/weather';
+  private apiUrl: string = 'https://api.openweathermap.org/data/2.5/';
 
   constructor(private http: HttpClient) {}
 
-  getWeather(city: string) {
-    return this.http.get(`${this.apiUrl}?q=${city}&appid=${this.apiKey}&units=metric`);
+  getWeather(city: string): Observable<WeatherData> {
+    return this.http.get(`${this.apiUrl}weather?q=${city}&appid=${this.apiKey}&units=metric`).pipe(
+      map(response => response as WeatherData) // Cast the response to WeatherData
+    );
   }
+  getHourlyWeather(lat: number, lon: number) {
+        return this.http.get(`${this.apiUrl}onecall?lat=${lat}&lon=${lon}&appid=${this.apiKey}&exclude=currentminutely,daily,alerts&units=metric`);
+  }
+
+
+  getFiveDayForecast(lat: number, lon: number): Observable<WeatherResponse> {
+    const url = `${this.apiUrl}forecast?lat=${lat}&lon=${lon}&appid=${this.apiKey}&units=metric`;
+    return this.http.get<WeatherResponse>(url);
+  }
+}
+interface WeatherIcon {
+  icon: string;
+}
+
+interface Forecast {
+  weather: WeatherIcon[];
+}
+interface ForecastResponse {
+  list: Forecast[];
+}
+export interface WeatherResponse {
+  list: {
+    weather: [
+      {
+        icon: string;
+        description:string;
+      }
+    ];
+    dt_txt:string;
+    main:
+     { temp_min:string;
+      temp_max:string;
+  }
+  }[];
+}
+
+export interface WeatherData {
+ coord:{
+  lat:number;
+  lon:number;
+ }
 }
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,31 +1,73 @@
-import { Component, OnInit } from '@angular/core';
-import { WeatherService } from '../weather.service';
+import { Component, OnInit,ViewEncapsulation, ViewChild, ElementRef } from '@angular/core';
+import { WeatherService,WeatherResponse, WeatherData } from '../weather.service';
+import { MatTabsModule } from '@angular/material/tabs';
+
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.css']
+  styleUrls: ['./weather.component.css'],
 })
+
 export class WeatherComponent implements OnInit {
   cities = ['Rio de Janeiro', 'Beijing', 'Los Angeles'];
   selectedCity!: string;
-  weatherData: any;
+  weatherData?: WeatherData;
+  hourlyWeather: any[] = [];
+  forecast!: WeatherResponse;
+
+  @ViewChild('searchInput') searchInputElement!: ElementRef;
 
   constructor(private weatherService: WeatherService) {}
 
   ngOnInit() {
     this.selectedCity = this.cities[0];
     this.getWeatherData(this.selectedCity);
+    this.searchCity(this.selectedCity);
   }
-
+  isWeatherArray(forcst: any): boolean {
+    return 'weather' in forcst && Array.isArray(forcst.weather);
+  }
   getWeatherData(city: string) {
-    this.weatherService.getWeather(city).subscribe(data => {
+    this.weatherService.getWeather(city).subscribe((data: WeatherData) => {
       this.weatherData = data;
+
+      this.weatherService.getFiveDayForecast(data.coord.lat, data.coord.lon).subscribe((forecastData: WeatherResponse) => {
+        this.forecast = forecastData;
+        console.log(this.forecast);
+      });
+
+      this.weatherService.getHourlyWeather(data.coord.lat,data.coord.lon).subscribe({
+        next: (response: any) => {
+          this.hourlyWeather = response.hourly;
+        },
+        error: (error) => {
+          console.error(error);
+        }
+      });
     });
   }
-
+  searchCity(city: string) {
+    this.weatherService.getHourlyWeather(39.9042, 116.4074).subscribe({
+      next: (response: any) => {
+        this.hourlyWeather = response.hourly;
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
   onCityChange(city: string) {
     this.selectedCity = city;
     this.getWeatherData(city);
   }
+  onSearchClick(value:string): void {
+    this.getWeatherData(value);
+  }
+  handleKeyPress(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.onSearchClick(this.searchInputElement.nativeElement.value);
+    }
+  }
+
 }
